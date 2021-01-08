10900c893d0aa3eb6133b11c9c0c7dfe2579e07c
sanchda <sanchda@users.noreply.github.com>
2021-01-08 15:44:44 +0000

    Fix implementation of stringtable demo

--- users/david.sanchez/native/src/demo_strings.c
+++ users/david.sanchez/native/src/demo_strings.c
@@ -1,28 +1,27 @@
+#include <stdio.h>
+
 #include "string_table.h"
 
 int main() {
   StringTable* st = stringtable_init();
   if(!st) return -1;
-  st->logging = 1; // enable logging to file
 
-  char w1[] = "";
-  char w2[] = "HELLO!";
-  char w3[] = "espresso";
-  char w4[] = "generatingfunctionology";
-
-  stringtable_add_cstr(st, w1);
-  stringtable_add_cstr(st, w2);
-  stringtable_add_cstr(st, w3);
-  stringtable_add_cstr(st, w4);
-
-  // Did it work?
-  printf("Index: %ld\n", stringtable_lookup_cstr(st, w1));
-  printf("Word: %s\n", stringtable_get(st, 1));
-
-  printf("Index: %ld\n", stringtable_lookup_cstr(st, w4));
-  printf("Word: %s\n", stringtable_get(st, 3));
+  FILE* fs = fopen("./words.txt", "r");
+  char* line = NULL; size_t len = 0;
+  ssize_t n;
+  size_t lines=0;
+  while(-1 != (n=getline(&line, &len, fs))) {
+    line[4] = 0;
+    if(-1 == stringtable_add(st, (unsigned char*)line, 4)) { // hardcoded length
+      printf("FAILURE\n");
+      return -1;
+    }
+    lines++;
+  }
+  fclose(fs);
 
   // And return everything
+  printf("Processed %ld lines\n", lines);
   stringtable_free(st);
   return 0;
 }
