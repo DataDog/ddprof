cafd533d1b66936813326b6a1b7d02c93026caf2
sanchda <sanchda@users.noreply.github.com>
2021-03-11 15:56:34 +0000

    Incremental update to build tooling

--- users/david.sanchez/native/.build_tools/clang_formatter.sh
+++ users/david.sanchez/native/.build_tools/clang_formatter.sh
@@ -3,22 +3,38 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-# Figure out latest clang-format command.
+# Figure out latest clang-format command.  This might be different than the
+# user's `clang-format` if they have one, but that's the point.
 CLANG_FORMAT=""
-if   command -v clang-format-11 >/dev/null 2>&1; then CLANG_FORMAT=clang-format-11
+if   command -v clang-format-12 >/dev/null 2>&1; then CLANG_FORMAT=clang-format-12
+elif command -v clang-format-11 >/dev/null 2>&1; then CLANG_FORMAT=clang-format-11
 elif command -v clang-format-10 >/dev/null 2>&1; then CLANG_FORMAT=clang-format-10
 elif command -v clang-format-9  >/dev/null 2>&1; then CLANG_FORMAT=clang-format-9
-else echo "No suitable clang-format found." && exit -1
+else echo "No suitable clang-format found." && exit 1
 fi
 
-for f in $(find . -name '*.h'   -o -name '*.cpp' -o -name '*.cxx' -o \
-                  -name '*.c++' -o -name '*.cc'  -o -name '*.cp'  -o \
-                  -name '*.c'   -o -name '*.i'   -o -name '*.ii'  -o \
-                  -name '*.h'   -o -name '*.h++' -o -name '*.hpp' -o \
-                  -name '*.hxx' -o -name '*.hh'  -o -name '*.inl' -o \
-                  -name '*.inc' -o -name '*.ipp' -o -name '*.ixx' -o \
-                  -name '*.txx' -o -name '*.tpp' -o -name '*.tcc' -o \
-                  -name '*.tpl')
-do
-  ${CLANG_FORMAT} -style=file "$@" $f
+RC=0
+APPLY="NO"
+[[ "$#" -ge "1" && "$1" == "apply" ]] && APPLY="YES"
+
+# Setup a tmpfile 
+tmpfile=$(mktemp /tmp/clang-format-diff.XXXXXX)
+exec 5<>$tmpfile
+rm -f $tmpfile
+tmpfile="/dev/fd/5"
+
+# Look for matching extensions and try to diff them.
+for f in $(git ls-files -m | grep -E '.*\.(c|cc|cp|cpp|cxx|c++|h|hh|hp|hpp|hxx|h++)$'); do
+  echo CHECKING $f
+  if [ ${APPLY} == "YES" ]; then
+    ${CLANG_FORMAT} -style=file -i $f
+  else
+    ${CLANG_FORMAT} -style=file $f > $tmpfile
+    if ! cmp -s $f /dev/fd/5; then
+      diff --color=always $f $tmpfile
+      RC=1
+    fi
+  fi
 done
+
+exit $RC
