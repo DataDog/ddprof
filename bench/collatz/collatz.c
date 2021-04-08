67f9d497f9a0b3bc8b2082cd35f26ca88a238e66
sanchda <sanchda@users.noreply.github.com>
2021-04-08 15:21:52 +0000

    Refine collatz benchmark
     * Add version number
     * --help or -h flags return help
     * --version or -v flags return version
     * New target settings to set a proscribed stack depth

--- bench/collatz/collatz.c
+++ bench/collatz/collatz.c
@@ -1,67 +1,66 @@
 #include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sys/mman.h>
+#include <sys/sysinfo.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <stdlib.h>
-#include <stdio.h>
+#include <unistd.h>
 #include <x86intrin.h>
+
+#define MYNAME "collatz"
+#define VER_MAJ 1
+#define VER_MIN 3
+#define VER_PATCH 1
+#ifndef VER_REV
+#  define VER_REV "custom"
+#endif
 
 unsigned long* counter = NULL;
 unsigned long* my_counter = &(unsigned long){0};
 
+// Helper for processing input
 #define P(s, d) ({                \
   long _t = strtoll(s, NULL, 10); \
   if (_t) d = _t;                 \
 })
-#define D(f) int f(int x)
-#define F(x) {                            \
+
+// This is the function body for every expanded function in the X-table
+#define FUNBOD {                          \
   int n = x&1 ? x*3+1 : x/2;              \
   __sync_add_and_fetch(my_counter, 1);    \
   return 1 >= n ? 1 : funs[n%funlen](n);  \
 }
 
-D(fun00); D(fun01); D(fun02); D(fun03); D(fun04); D(fun05); D(fun06); D(fun07); D(fun08); D(fun09);
-D(fun10); D(fun11); D(fun12); D(fun13); D(fun14); D(fun15); D(fun16); D(fun17); D(fun18); D(fun19);
-D(fun20); D(fun21); D(fun22); D(fun23); D(fun24); D(fun25); D(fun26); D(fun27); D(fun28); D(fun29);
-D(fun30); D(fun31); D(fun32); D(fun33); D(fun34); D(fun35); D(fun36); D(fun37); D(fun38); D(fun39);
-D(fun40); D(fun41); D(fun42); D(fun43); D(fun44); D(fun45); D(fun46); D(fun47); D(fun48); D(fun49);
-D(fun50); D(fun51); D(fun52); D(fun53); D(fun54); D(fun55); D(fun56); D(fun57); D(fun58); D(fun59);
-D(fun60); D(fun61); D(fun62); D(fun63); D(fun64); D(fun65); D(fun66); D(fun67); D(fun68); D(fun69);
-D(fun70); D(fun71); D(fun72); D(fun73); D(fun74); D(fun75); D(fun76); D(fun77); D(fun78); D(fun79);
-D(fun80); D(fun81); D(fun82); D(fun83); D(fun84); D(fun85); D(fun86); D(fun87); D(fun88); D(fun89);
-D(fun90); D(fun91); D(fun92); D(fun93); D(fun94); D(fun95); D(fun96); D(fun97); D(fun98); D(fun99);
+// Macro system for recursively expanding concatenated names into an
+// X-table
+#define N1(X,f) X(f)
+#define N2(X,f) N1(X,f##0) N1(X,f##1) N1(X,f##2) N1(X,f##3) N1(X,f##4) N1(X,f##5) N1(X,f##6) N1(X,f##7) N1(X,f##8) N1(X,f##9)
+#define N3(X,f) N2(X,f##0) N2(X,f##1) N2(X,f##2) N2(X,f##3) N2(X,f##4) N2(X,f##5) N2(X,f##6) N2(X,f##7) N2(X,f##8) N2(X,f##9)
+#define C(X,N) N(X,f0) N(X,f1) N(X,f2) N(X,f3) N(X,f4) N(X,f5) N(X,f6) N(X,f7) N(X,f8) N(X,f9)
 
+// X-table; use something like gcc -E collatz.c to see how this works :)
+#define COLLATZ(X) C(X,N3)
+#define DECL(f) int f(int);
+#define NAME(f) f,
+#define DEFN(f) int f(int x) FUNBOD;
+
+// Declare function prototypes
+COLLATZ(DECL)
+
+// Define function lookup table
 int (*funs[])(int) = {
-  fun00, fun01, fun02, fun03, fun04, fun05, fun06, fun07, fun08, fun09,
-  fun10, fun11, fun12, fun13, fun14, fun15, fun16, fun17, fun18, fun19,
-  fun20, fun21, fun22, fun23, fun24, fun25, fun26, fun27, fun28, fun29,
-  fun30, fun31, fun32, fun33, fun34, fun35, fun36, fun37, fun38, fun39,
-  fun40, fun41, fun42, fun43, fun44, fun45, fun46, fun47, fun48, fun49,
-  fun50, fun51, fun52, fun53, fun54, fun55, fun56, fun57, fun58, fun59,
-  fun60, fun61, fun62, fun63, fun64, fun65, fun66, fun67, fun68, fun69,
-  fun70, fun71, fun72, fun73, fun74, fun75, fun76, fun77, fun78, fun79,
-  fun80, fun81, fun82, fun83, fun84, fun85, fun86, fun87, fun88, fun89,
-  fun90, fun91, fun92, fun93, fun94, fun95, fun96, fun97, fun98, fun99,
+  COLLATZ(NAME)
 };
-
-#include <sys/sysinfo.h>
-#include <sys/types.h>
-#include <unistd.h>
-
 const int funlen = sizeof(funs) / sizeof(*funs);
 
-D(fun00) F(x); D(fun01) F(x); D(fun02) F(x); D(fun03) F(x); D(fun04) F(x); D(fun05) F(x); D(fun06) F(x); D(fun07) F(x); D(fun08) F(x); D(fun09) F(x);
-D(fun10) F(x); D(fun11) F(x); D(fun12) F(x); D(fun13) F(x); D(fun14) F(x); D(fun15) F(x); D(fun16) F(x); D(fun17) F(x); D(fun18) F(x); D(fun19) F(x);
-D(fun20) F(x); D(fun21) F(x); D(fun22) F(x); D(fun23) F(x); D(fun24) F(x); D(fun25) F(x); D(fun26) F(x); D(fun27) F(x); D(fun28) F(x); D(fun29) F(x);
-D(fun30) F(x); D(fun31) F(x); D(fun32) F(x); D(fun33) F(x); D(fun34) F(x); D(fun35) F(x); D(fun36) F(x); D(fun37) F(x); D(fun38) F(x); D(fun39) F(x);
-D(fun40) F(x); D(fun41) F(x); D(fun42) F(x); D(fun43) F(x); D(fun44) F(x); D(fun45) F(x); D(fun46) F(x); D(fun47) F(x); D(fun48) F(x); D(fun49) F(x);
-D(fun50) F(x); D(fun51) F(x); D(fun52) F(x); D(fun53) F(x); D(fun54) F(x); D(fun55) F(x); D(fun56) F(x); D(fun57) F(x); D(fun58) F(x); D(fun59) F(x);
-D(fun60) F(x); D(fun61) F(x); D(fun62) F(x); D(fun63) F(x); D(fun64) F(x); D(fun65) F(x); D(fun66) F(x); D(fun67) F(x); D(fun68) F(x); D(fun69) F(x);
-D(fun70) F(x); D(fun71) F(x); D(fun72) F(x); D(fun73) F(x); D(fun74) F(x); D(fun75) F(x); D(fun76) F(x); D(fun77) F(x); D(fun78) F(x); D(fun79) F(x);
-D(fun80) F(x); D(fun81) F(x); D(fun82) F(x); D(fun83) F(x); D(fun84) F(x); D(fun85) F(x); D(fun86) F(x); D(fun87) F(x); D(fun88) F(x); D(fun89) F(x);
-D(fun90) F(x); D(fun91) F(x); D(fun92) F(x); D(fun93) F(x); D(fun94) F(x); D(fun95) F(x); D(fun96) F(x); D(fun97) F(x); D(fun98) F(x); D(fun99) F(x);
+// Define the functions
+COLLATZ(DEFN)
+
 
 #define MAX_PROCS 1000
+// Program entrypoint
 int main (int c, char** v) {
   // Define and ingest parameters
   int ki = 1e1;
@@ -69,6 +68,23 @@ int main (int c, char** v) {
   int t = 0;
   int n = 1+get_nprocs()/2;
   if (c > 1) {
+    if (!strcmp(v[1], "-v") || !strcmp(v[1], "--version")) {
+      printf(MYNAME" v%d.%d.%d-%s\n", VER_MAJ, VER_MIN, VER_PATCH, VER_REV);
+      return 0;
+    } else if (!strcmp(v[1], "-h") || !strcmp(v[1], "--help")) {
+      printf("collatz <CPUs> <outer index> <inner index> <target value>\n");
+      printf("  CPUs -- number of CPUs to use (defaults to 1/2 + 1 of total)\n");
+      printf("  outer/inner indices -- outer*inner = total loops\n");
+      printf("  target -- value for collatz conjecture; otherwise uses every index from inner loop\n");
+      printf("    Also supports the following special values (val; depth):\n");
+      printf("      A -- (7; 16)\n");
+      printf("      B -- (27; 111)\n");
+      printf("      C -- (703; 170)\n");
+      printf("      D -- (2463; 208)\n");
+      printf("      E -- (6171; 261)\n");
+      printf("    These values are from https://oeis.org/A006577/b006577.txt (table.txt)\n");
+      return 0;
+    }
     P(v[1], n);
     if (n > MAX_PROCS) n = MAX_PROCS;
   }
@@ -76,8 +92,17 @@ int main (int c, char** v) {
     P(v[2], ki);
   if (c > 3)
     P(v[3], kj);
-  if (c > 4)
-    P(v[4], t);
+  if (c > 4) {
+    switch(*v[4]) {
+      case 'A': case 'a': t = 7;    break;
+      case 'B': case 'b': t = 27;   break;
+      case 'C': case 'c': t = 703;  break;
+      case 'D': case 'd': t = 2463; break;
+      case 'E': case 'e': t = 6171; break;
+      default:
+        P(v[4], t);
+    }
+  }
   printf("%d, %d, %d, %d, ", n, ki, kj, t); fflush(stdout);
 
   // Setup
@@ -107,7 +132,7 @@ int main (int c, char** v) {
   pthread_barrier_wait(pb);
   for (int j=0; j<ki; j++)
     for (int i=0; i<kj; i++)
-      fun00(t ? t : i);
+      funs[0](t ? t : i);
 
   // Wait for everyone to be done
   __sync_add_and_fetch(counter,*my_counter);
