299e9e5623402cdfdd4cafd84634b35cf9ba2680
sanchda <sanchda@users.noreply.github.com>
2021-04-12 19:44:56 +0000

    Fix up tracked git hooks and installer
    Not very nice, but whatever

--- hooks/install.sh
+++ hooks/install.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-# Installs hook symlinks
-ln -s hooks/pre-commit .git/hooks/pre-commit
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")") # root dir?
+HOOKS="pre-commit"
+HOOK_DIR=$(git rev-parse --show-toplevel)/.git/hooks
+
+for h in $HOOKS; do
+  # TODO move into an archive?
+  [ -f $HOOK_DIR/$h ] && rm $HOOK_DIR/$h
+
+  ln -s -f $SCRIPT_DIR/$h $HOOK_DIR/$h
+done
