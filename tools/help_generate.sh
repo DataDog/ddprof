b09f23511b5bfcd1409c397069796875a2b361e2
sanchda <sanchda@users.noreply.github.com>
2021-04-19 15:38:09 +0000

    Add online help and help generation
    The online help for ddprof is generated at time of execution, since we
    have a tight binding between the commandline options, environment
    variables, the help strings, and all of that kind of stuff.  Rather than
    generating the code dynamically from config (in which case, we could
    generate the help from there too), we keep config-as-code in a tight,
    tabular format and generate the help at runtime.
    
    This adds a script that generates a dumb skeleton .md in docs and just
    passes the output of the ddprof help command into the file.  The user
    still needs to actually run this step and commit the results and junk.

---
+++ tools/help_generate.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# http://redsymbol.net/articles/unofficial-bash-strict-mode/
+set -euo pipefail
+IFS=$'\n\t'
+
+FILE=$(git rev-parse --show-toplevel)/docs/Commands.md
+echo "# ddprof Commands" > ${FILE}
+echo "" >> ${FILE}
+echo "" >> ${FILE}
+echo '```' >> ${FILE}
+release/ddprof >> ${FILE}
+echo '```' >> ${FILE}
