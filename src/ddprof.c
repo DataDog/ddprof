fce1780357d6e9e1c9a1f9fdce79a5c16ace7652
sanchda <sanchda@users.noreply.github.com>
2021-05-21 17:02:25 +0000

    Enable HTTP keepalive again

--- src/ddprof.c
+++ src/ddprof.c
@@ -418,7 +418,7 @@ int main(int argc, char **argv) {
       &(struct DDProfContext){.ddr = &(DDReq){.user_agent = "libddprof001",
                                               .language = "native",
                                               .family = "native",
-                                              .http_close = true},
+                                              .http_close = false},
                               .dp = &(DProf){0},
                               .us = &(struct UnwindState){0}};
   DDReq *ddr = ctx->ddr;
