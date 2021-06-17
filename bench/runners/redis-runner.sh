93e13d21670d5e23d67184bce3faa5975a3897fe
sanchda <sanchda@users.noreply.github.com>
2021-06-17 19:42:14 +0000

    Moved demo resources out of test.
    We'll gradually move these into docker or something, but for now let's
    reduce the clutter in the test/ directory
    
    Also update pinned libddprof

--- bench/runners/redis-runner.sh
+++ bench/runners/redis-runner.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-redis-server test/redis.conf > /dev/null 2>&1 &
+redis-server demos/redis.conf > /dev/null 2>&1 &
 #fio --filename=fio --size=5MB --rw=randrw --bs=4k --ioengine=libaio --iodepth=256 --runtime=120 --numjobs=4 --time_based --group_reporting --name=iops-test-job --eta-newline=1 &
 sleep 1
 redis-benchmark -q -l > /dev/null 2>&1
