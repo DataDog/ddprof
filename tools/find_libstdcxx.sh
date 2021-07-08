4cc6f47b7deae879660317fb53d62035949f3b49
sanchda <sanchda@users.noreply.github.com>
2021-07-08 09:41:34 -0500

    Fixes to tools/find_libstdcxx.sh
    It turns out that it's possible to have a GCC installation without a
    corresponding libstdc++.  Not only that, we have such an installation in
    our CI process.

--- tools/find_libstdcxx.sh
+++ tools/find_libstdcxx.sh
@@ -14,15 +14,25 @@ cd $CURRENTDIR
 # This should always point to the location of libstdc++ specified by gcc.
 # It's possible that gcc or the passed parameter doesn't exist, but for clarity
 # we want errors to be thrown right here.
-GCC=$(command -v gcc{-12,-11,-10,-9,-8,} | head -n 1)
+RETVAL=""
+for c in $(command -v gcc{-12,-11,-10,-9,-8,}); do
+  LIBSTDCXX_FILE=$(dirname $($c -print-libgcc-file-name))/libstdc++.a
+  if [ -f "$LIBSTDCXX_FILE" ]; then
+    RETVAL="$LIBSTDCXX_FILE"
+    break;
+  fi
+done
 if [ ! -z ${1:-} ]; then
   if command -v ${1} > /dev/null; then
-  GCC=${1}
+    LIBSTDCXX_FILE=$(dirname $(${1} -print-libgcc-file-name))/libstdc++.a
+    if [ -f "$LIBSTDCXX_FILE" ]; then
+      RETVAL="$LIBSTDCXX_FILE"
+      break;
+    fi
   fi
 fi
-if [ -z ${GCC:-} ]; then
-  echo "No suitable GCC found or given" >&2
-  GCC=gcc
+if [ -z ${RETVAL} ]; then
+  echo "No suitable libstdc++.a found"
 fi
 
-echo $(dirname $(${GCC} -print-libgcc-file-name))/libstdc++.a
+echo $RETVAL
