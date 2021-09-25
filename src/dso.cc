05bc3f413139c1bce3299e0b91351e20d4cff0db
sanchda <sanchda@users.noreply.github.com>
2021-09-25 09:25:27 +0000

    use std::make_pair to fix DsoFindRes initialization

--- src/dso.cc
+++ src/dso.cc
@@ -372,9 +372,7 @@ void DsoHdr::erase_range(const DsoRange &range) {
 }
 
 DsoFindRes DsoHdr::dso_find_same_or_smaller(const ddprof::Dso &dso) {
-  DsoFindRes res = {};
-  res.first = _set.find(dso);
-  res.second = false;
+  DsoFindRes res = std::make_pair<DsoFindRes::first_type, DsoFindRes::second_type>(_set.find(dso), false);
   // comparator only looks at start ptr
   if (res.first != _set.end()) {
     // if it is the same or smaller, we keep the current dso
