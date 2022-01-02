c85c9e00191f8473f3e4b4a162a45d908128779b
r1viollet <74836499+r1viollet@users.noreply.github.com>
2022-01-02 21:38:50 +0100

    Move CI process to ddprof-build (#219)
    Tests, runners, coverage and benchmarks moved to ddprof-build.

--- bench/runners/redis-runner.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Unless explicitly stated otherwise all files in this repository are licensed under the Apache License Version 2.0.
-# This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2021-Present Datadog, Inc.
-
-redis-server demos/redis.conf > /dev/null 2>&1 &
-sleep 1
-redis-benchmark -q -l > /dev/null 2>&1
