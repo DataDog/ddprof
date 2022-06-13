4357570a1e079c9b37ad8d92d284d3fa8073e79b
Nicolas Savoire <nicolas.savoire@datadoghq.com>
2022-06-13 18:38:59 +0000

    Attempt to fix IPC test
    * Make sure that child fork exits
    * Add a 10ms tolerance
    * Retry send/recv when interrupted wiht EINTR
    * Handle partial read/writes
    * Avoid using MSG_DONTWAIT (redundant with timeout)

--- include/ddprof_buffer.hpp
+++ include/ddprof_buffer.hpp
@@ -2,10 +2,17 @@
 // under the Apache License Version 2.0. This product includes software
 // developed at Datadog (https://www.datadoghq.com/). Copyright 2021-Present
 // Datadog, Inc.
+#pragma once
 
 #include "span.hpp"
 
 namespace ddprof {
 using Buffer = ddprof::span<std::byte>;
 using ConstBuffer = ddprof::span<const std::byte>;
+
+template <typename T>
+ddprof::span<T> remaining(ddprof::span<T> buffer, size_t offset) {
+  offset = std::min(offset, buffer.size());
+  return {buffer.data() + offset, buffer.size() - offset};
+}
 } // namespace ddprof
