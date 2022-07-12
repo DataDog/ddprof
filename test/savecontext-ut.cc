5c42c6620b569c23101c5351fdc99f18fc9a516e
Nicolas Savoire <nicolas.savoire@datadoghq.com>
2022-07-12 10:46:46 +0000

    Add unit test for unwinding in signal handler
    Test fix in add_symbol for calling dwfl_frame_pc twice: first without isactivation info to just get pc, then register mod if needed and call dwfl_frame_pc a second time with isactivation.

--- test/savecontext-ut.cc
+++ test/savecontext-ut.cc
@@ -5,6 +5,8 @@
 
 #include <gtest/gtest.h>
 
+#include "ddprof_base.hpp"
+#include "defer.hpp"
 #include "perf.hpp"
 #include "savecontext.hpp"
 #include "unwind.hpp"
@@ -13,8 +15,8 @@
 #include <algorithm>
 #include <unistd.h>
 
-void funcA() __attribute__((noinline));
-void funcB() __attribute__((noinline));
+DDPROF_NOINLINE void funcA();
+DDPROF_NOINLINE void funcB();
 
 std::byte stack[PERF_SAMPLE_STACK_SIZE];
 
@@ -45,8 +47,46 @@ void funcB() {
 
 void funcA() {
   funcB();
-  // prevent tail call optimization
-  getpid();
+  DDPROF_BLOCK_TAIL_CALL_OPTIMIZATION();
 }
 
 TEST(getcontext, getcontext) { funcA(); }
+
+DDPROF_NOINLINE void funcD() { asm("int3"); }
+
+DDPROF_NOINLINE void funcC() {
+  funcD();
+  DDPROF_BLOCK_TAIL_CALL_OPTIMIZATION();
+}
+
+static uint64_t regs[K_NB_REGS_UNWIND];
+static size_t stack_size;
+
+void handler(int sig) { stack_size = save_context(regs, stack); }
+
+TEST(getcontext, unwind_from_sighandler) {
+  signal(SIGTRAP, &handler);
+  defer { signal(SIGTRAP, SIG_DFL); };
+  funcC();
+  UnwindState state;
+  ddprof::unwind_init_sample(&state, regs, getpid(), stack_size,
+                             reinterpret_cast<char *>(stack));
+  ddprof::unwindstate__unwind(&state);
+
+  auto &symbol_table = state.symbol_hdr._symbol_table;
+
+  for (size_t iloc = 0; iloc < state.output.nb_locs; ++iloc) {
+    auto &symbol = symbol_table[state.output.locs[iloc]._symbol_idx];
+    printf("%zu: %s\n", iloc, symbol._demangle_name.c_str());
+  }
+
+  EXPECT_GT(state.output.nb_locs, 5);
+  auto &symbol0 = symbol_table[state.output.locs[0]._symbol_idx];
+  EXPECT_TRUE(symbol0._demangle_name.starts_with("save_context("));
+  auto &symbol1 = symbol_table[state.output.locs[1]._symbol_idx];
+  EXPECT_EQ(symbol1._demangle_name, "handler(int)");
+  auto &symbol3 = symbol_table[state.output.locs[3]._symbol_idx];
+  EXPECT_EQ(symbol3._demangle_name, "funcD()");
+  auto &symbol4 = symbol_table[state.output.locs[4]._symbol_idx];
+  EXPECT_EQ(symbol4._demangle_name, "funcC()");
+}
