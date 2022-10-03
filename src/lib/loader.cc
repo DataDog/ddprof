b92bf1d4858496cf612e2f02f648aefc412cf988
Nicolas Savoire <nicolas.savoire@datadoghq.com>
2022-10-03 11:25:31 +0000

    Document loader mechanism

--- src/lib/loader.cc
+++ src/lib/loader.cc
@@ -13,7 +13,6 @@
 
 #include <dlfcn.h>
 #include <fcntl.h>
-#include <filesystem>
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -21,6 +20,30 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+/* Role of loader is to ensure that all dependencies (libdl/lim/lipthread) of
+ * libdd_profiling-embedded.so are satisfied before dlopening it.
+ * On musl, all libc features are in libc.so and hence are available once libc
+ * is loaded.
+ * Whereas on glibc, some features are in their own lib (this is
+ * changing with recent glibc versions, where most features are moved back to
+ * libc.so.6):
+ * - libdl: dlopen/dlsym
+ * - libm: math functions (log,...)
+ * - libpthread: threading functions (pthread_create,...)
+ * Therefore when executing on glibc, some required dependencies
+ * (dlopen/dlsym/pthread/log) of libdd_profiling might not be loaded, that's why
+ * libdd_profiling is indirectly loaded by loader after ensuring that these
+ * dependencies are available.
+ * Note that libdd_profiling cannot depend on libdl/libm/libpthread since those
+ * do not exist on musl. Even libc has a different name (libc.so.6 vs
+ * libc.musl-x86_64.so.1), that is why libloader and libdd_profiling do not
+ * depend explicitly on libc but rely on the target process loading libc (this
+ * will not work if target process does not depend on libc, but in that case,
+ * libdd_profiling will not be able to intercept allocations anyway).
+ * Loader load missing dependencies by dlopening them., since dlopen may not be
+ * available, it falls back to using __libc_dlopen_mode, an internal libc.so.6
+ * function implementing dlopen, in that case.
+ */
 extern "C" {
 void *dlopen(const char *filename, int flags) noexcept __attribute__((weak));
 void *dlsym(void *handle, const char *symbol) noexcept __attribute__((weak));
@@ -39,11 +62,14 @@ static void *my_dlopen(const char *filename, int flags) {
   static decltype(dlopen) *dlopen_ptr = &dlopen;
 
   if (!dlopen_ptr) {
+    // if libdl.so is not loaded, use __libc_dlopen_mode
     dlopen_ptr = __libc_dlopen_mode;
   }
   if (dlopen_ptr) {
     return dlopen_ptr(filename, flags);
   }
+
+  // Should not happen
   return NULL;
 }
 
@@ -68,14 +94,24 @@ static void ensure_libpthread_is_loaded() {
 static void *my_dlsym(void *handle, const char *symbol) {
   static decltype(dlsym) *dlsym_ptr = &dlsym;
   if (!dlsym_ptr) {
+    // dlysm is not available: meaning we are on glibc and libdl.so was not
+    // loaded at startup
+
+    // First ensure that libdl.so is loaded
     if (!s_libdl_handle) {
       ensure_libdl_is_loaded();
     }
 
     if (s_libdl_handle) {
+      // locate dlsym in libdl.so by using internal libc.so.6 function
+      // __libc_dlsym.
+      // Note that we need dlsym because __libc_dlsym does not provide
+      // RTLD_DEFAULT/RTLD_NEXT functionality.
       dlsym_ptr = reinterpret_cast<decltype(dlsym) *>(
           __libc_dlsym(s_libdl_handle, "dlsym"));
     }
+
+    // Should not happen
     if (!dlsym_ptr) {
       return NULL;
     }
@@ -86,7 +122,7 @@ static void *my_dlsym(void *handle, const char *symbol) {
 
 typedef int (*FstatFunc)(int, struct stat *) noexcept;
 
-// fstat is linked statically on glibc and  symbol is not present in libc.so.6
+// fstat is linked statically on glibc and symbol is not present in libc.so.6
 // Provide a replacement that calls __fxstat is present or fstat resolved with
 // dlsym/RTLD_NEXT
 extern "C" int __fxstat(int ver, int fd, struct stat *buf)
@@ -98,15 +134,20 @@ extern int fstat(int fd, struct stat *buf)
 // NOLINTNEXTLINE cert-dcl51-cpp
 extern "C" __attribute__((unused)) int __fstat(int fd, struct stat *buf) {
   if (__fxstat) {
+    // __fxstat is available call it directly
     return __fxstat(1, fd, buf);
   }
-  static __typeof(fstat) *s_fstat = NULL;
+  // __fxtstat is not available, we must be executing on musl, therefore `fstat`
+  // should be availble in libc
+  static declatype(fstat) *s_fstat = NULL;
   if (s_fstat == NULL) {
-    s_fstat = (FstatFunc)my_dlsym(RTLD_NEXT, "fstat");
+    s_fstat = reinterpret_cast<FstatFunc>(my_dlsym(RTLD_NEXT, "fstat"));
   }
   if (s_fstat) {
     return s_fstat(fd, buf);
   }
+
+  // Should not happen
   return -1;
 }
 
@@ -157,6 +198,9 @@ static void __attribute__((destructor)) unloader() {
   }
 }
 
+// We need to provide libdd_profiling-embedded.so interface and forward to it
+// because user needs to be able to link with libdd_profiling.so (which is just
+// the loader).
 int ddprof_start_profiling() {
   return s_start_profiling_func ? s_start_profiling_func() : -1;
 }
