5110759d823abc58696d9696b7d0b57b051a2f20
sanchda <sanchda@users.noreply.github.com>
2022-10-19 09:16:46 -0500

    Update licenses

--- util/build.sh
+++ util/build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Unless explicitly stated otherwise all files in this repository are licensed under the Apache License Version 2.0.
+# This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2021-Present Datadog, Inc.
 set -euo pipefail 
 
 ONAME="event_parser"
