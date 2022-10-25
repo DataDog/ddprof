da7816e5ff023b398072a6050b98c3cca0295980
sanchda <sanchda@users.noreply.github.com>
2022-10-25 00:14:29 -0500

    Style fixups

--- src/perf_archmap.cc
+++ src/perf_archmap.cc
@@ -37,9 +37,11 @@ unsigned int param_to_perf_regno(unsigned int param_no) {
 // Populate lookups for converting parameter number (1-indexed) to regno
 #define R(x) REGNAME(x)
 #ifdef __x86_64__
-  constexpr int reg_lookup[] = {-1, R(RDI), R(RSI), R(RDX), R(RCX), R(R8), R(R9)};
+  constexpr int reg_lookup[] = {-1,     R(RDI), R(RSI), R(RDX),
+                                R(RCX), R(R8),  R(R9)};
 #elif __aarch64__
-  constexpr int reg_lookup[] = {-1, R(X0), R(X1), R(X2), R(X3), R(X4), R(X5), R(X6)};
+  constexpr int reg_lookup[] = {-1,    R(X0), R(X1), R(X2),
+                                R(X3), R(X4), R(X5), R(X6)};
 #else
 // cppcheck-suppress preprocessorErrorDirective
 #  error Architecture not supported
