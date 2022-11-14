74065b48e6cf64947f43a7643e3c4d40396066d3
r1viollet <74836499+r1viollet@users.noreply.github.com>
2022-11-14 22:37:11 +0100

    Allow usage of jemalloc in ddprof (#207)
    * Allow usage of jemalloc in ddprof
    * Allow installation of jemalloc on alpine
    * Add a documentation note to check on jemalloc stats

--- src/dwfl_thread_callbacks.cc
+++ src/dwfl_thread_callbacks.cc
@@ -19,6 +19,7 @@ pid_t next_thread(Dwfl *dwfl, void *arg, void **thread_argp) {
   return us->pid;
 }
 
+
 // DWARF and the Linux kernel don't have a uniform view of the processor, so we
 // can't just follow the register mask and shove it into the output registers.
 // Instead, we crib off of libdwfl's ARM/x86 unwind code in elfutil's
@@ -26,13 +27,13 @@ pid_t next_thread(Dwfl *dwfl, void *arg, void **thread_argp) {
 bool set_initial_registers(Dwfl_Thread *thread, void *arg) {
   Dwarf_Word regs[PERF_REGS_COUNT] = {}; // max register count across all arcs
   struct UnwindState *us = reinterpret_cast<UnwindState *>(arg);
-
+  // clang-format off
   unsigned int regs_num;
   for (regs_num = 0; - 1u != dwarf_to_perf_regno(regs_num); ++regs_num) {
     unsigned int regs_idx = dwarf_to_perf_regno(regs_num);
     regs[regs_num] = us->initial_regs.regs[regs_idx];
   }
-
+  // clang-format on
   // Although the perf registers designate the register after SP as the PC, this
   // convention is not a documented convention of the DWARF registers.  We set
   // the PC manually.
