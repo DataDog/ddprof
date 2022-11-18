fcda4a5ee8f5a5117923ad824940e33fc5979e5e
r1viollet <erwan.viollet@datadoghq.com>
2022-11-18 14:44:36 +0100

    Create a remote unwinding test

--- test/dwarf_unwind-ut.cc
+++ test/dwarf_unwind-ut.cc
@@ -32,7 +32,6 @@ DDPROF_NOINLINE size_t funcA(std::array<uint64_t, PERF_REGS_COUNT> &regs);
 DDPROF_NOINLINE size_t funcB(std::array<uint64_t, PERF_REGS_COUNT> &regs);
 
 size_t funcB(std::array<uint64_t, PERF_REGS_COUNT> &regs) {
-
   printf("Here we are in B %lx \n", _THIS_IP_);
   size_t size = save_context(retrieve_stack_end_address(), regs, stack);
 
@@ -74,4 +73,83 @@ TEST(dwarf_unwind, simple) {
   ASSERT_TRUE(std::string(syms[0]).find("save_context") != std::string::npos);
   ASSERT_TRUE(std::string(syms[1]).find("funcB") != std::string::npos);
   ASSERT_TRUE(std::string(syms[2]).find("funcA") != std::string::npos);
+}
+
+
+
+#include "ringbuffer_holder.hpp"
+#include "ringbuffer_utils.hpp"
+#include "allocation_tracker.hpp"
+#include "perf_ringbuffer.hpp"
+
+DDPROF_NOINLINE void func_save_sleep(size_t size);
+DDPROF_NOINLINE void func_intermediate(size_t size);
+
+DDPROF_NOINLINE void func_save_sleep(size_t size) {
+  ddprof::AllocationTracker::track_allocation(0xdeadbeef, size);
+  // prevent tail call optimization
+  getpid();
+  sleep(1);
+}
+
+void func_intermediate(size_t size) {
+    func_save_sleep(size);
+}
+
+TEST(dwarf_unwind, remote) {
+  const uint64_t rate = 1;
+  const size_t buf_size_order = 5;
+  ddprof::RingBufferHolder ring_buffer{buf_size_order,
+                                       RingBufferType::kMPSCRingBuffer};
+  // use allocation tracking to store events
+  ddprof::AllocationTracker::allocation_tracking_init(
+      rate, ddprof::AllocationTracker::kDeterministicSampling,
+      ring_buffer.get_buffer_info());
+
+  // Fork
+  pid_t parent_pid = getpid();
+  pid_t temp_pid = fork();
+  if (!temp_pid) {
+    func_intermediate(10);
+    return;
+  }
+
+  // Load libraries from the fork - Cache array is relent to a single pid
+  CodeCacheArray cache_arary;
+  Symbols::parsePidLibraries(temp_pid, &cache_arary, false);
+  // Establish a ring buffer ?
+
+  ddprof::MPSCRingBufferReader reader{ring_buffer.get_ring_buffer()};
+  ASSERT_GT(reader.available_size(), 0);
+
+  auto buf = reader.read_sample();
+  ASSERT_FALSE(buf.empty());
+  const perf_event_header *hdr =
+      reinterpret_cast<const perf_event_header *>(buf.data());
+  ASSERT_EQ(hdr->type, PERF_RECORD_SAMPLE);
+
+  // convert based on mask for this watcher (default in this case)
+  perf_event_sample *sample = hdr2samp(hdr, perf_event_default_sample_type());
+
+  std::span<uint64_t, PERF_REGS_COUNT> regs_span{sample->regs, PERF_REGS_COUNT};
+  ap::StackContext sc = ap::from_regs(regs_span);
+  std::span<std::byte> stack{ reinterpret_cast<std::byte*>(sample->data_stack), sample->size_stack};
+  ap::StackBuffer buffer(stack, sc.sp, sc.sp + sample->size_stack);
+
+  void *callchain[DD_MAX_STACK_DEPTH];
+  int n = stackWalk(&cache_arary, sc, buffer, const_cast<const void **>(callchain),
+                    DD_MAX_STACK_DEPTH, 0);
+
+  std::array<const char*, DD_MAX_STACK_DEPTH> syms;
+  for (int i = 0; i < n; ++i) {
+    { // retrieve symbol
+      CodeCache *code_cache = findLibraryByAddress(
+          &cache_arary, reinterpret_cast<void *>(callchain[i]));
+      if (code_cache) {
+        syms[i] = code_cache->binarySearch(callchain[i]);
+        printf("IP = %p - %s\n", callchain[i], syms[i]);
+      }
+    }
+  }
+
 }
