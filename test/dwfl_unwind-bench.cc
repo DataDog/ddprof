a14835f382be9b8c20962c0dcc2ad049e2628eac
r1viollet <erwan.viollet@datadoghq.com>
2022-11-22 13:20:20 +0100

    Prevent tail call optimisation

--- test/dwfl_unwind-bench.cc
+++ test/dwfl_unwind-bench.cc
@@ -17,22 +17,29 @@ DDPROF_NOINLINE size_t func_save(ddprof::span<std::byte> stack, ddprof::span<uin
 DDPROF_NOINLINE size_t func_intermediate_1(int i, ddprof::span<std::byte> stack, ddprof::span<uint64_t, PERF_REGS_COUNT> regs);
 
 DDPROF_NOINLINE size_t func_save(ddprof::span<std::byte> stack, ddprof::span<uint64_t, PERF_REGS_COUNT> regs) {
-  return save_context(retrieve_stack_end_address(), regs, stack);
+  size_t size = save_context(retrieve_stack_end_address(), regs, stack);
+  DDPROF_BLOCK_TAIL_CALL_OPTIMIZATION();
+  return size;
 }
 
 DDPROF_NOINLINE size_t func_intermediate_1(int i, ddprof::span<std::byte> stack, ddprof::span<uint64_t, PERF_REGS_COUNT> regs) {
   while(i > 0){
-    return func_intermediate_1(--i, stack, regs);
+    size_t size = func_intermediate_1(--i, stack, regs);
+    DDPROF_BLOCK_TAIL_CALL_OPTIMIZATION();
+    return size;
   }
-  return func_save(stack, regs);
+  size_t size = func_save(stack, regs);
+  DDPROF_BLOCK_TAIL_CALL_OPTIMIZATION();
+  return size;
+
 }
 
-static void BM_SaveContext(benchmark::State &state) {
+static void BM_UnwindSameStack(benchmark::State &state) {
   UnwindState unwind_state;
   std::byte stack[PERF_SAMPLE_STACK_SIZE];
   std::array<uint64_t, PERF_REGS_COUNT> regs;
 
-  constexpr int depth_walk = 10;
+  constexpr unsigned depth_walk = 10;
   pid_t mypid = getpid();
   int cpt = 0;
   for (auto _ : state) {
@@ -45,16 +52,15 @@ static void BM_SaveContext(benchmark::State &state) {
     ddprof::unwindstate__unwind(&unwind_state);
 
     if (unlikely(unwind_state.output.nb_locs < depth_walk)) {
-      printf("Exit as unwind output = %d \n", unwind_state.output.nb_locs);
-//      exit(1);
+      printf("Exit as unwind output = %lu \n", unwind_state.output.nb_locs);
+      exit(1);
     }
     const auto &symbol_table = unwind_state.symbol_hdr._symbol_table;
 
-    for (int i = 0; i < unwind_state.output.nb_locs; ++i) {
+    for (unsigned i = 0; i < unwind_state.output.nb_locs; ++i) {
       {
         const auto &symbol =
             symbol_table[unwind_state.output.locs[i]._symbol_idx];
-//        printf("IP[%d] = %s\n", i, symbol._demangle_name.c_str());
         cpt += symbol._demangle_name.size();
       }
     }
@@ -62,4 +68,4 @@ static void BM_SaveContext(benchmark::State &state) {
   printf("cpt = %d", cpt);
 }
 
-BENCHMARK(BM_SaveContext);
+BENCHMARK(BM_UnwindSameStack);
