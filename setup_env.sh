3670cf14224fb7f3bb69158d8a0cf4c2c8bfb197
r1viollet <erwan.viollet@datadoghq.com>
2023-02-21 16:19:51 +0100

    OS upgrade - minor setup_env fix
    Fix the scope of bash variables

--- setup_env.sh
+++ setup_env.sh
@@ -26,9 +26,6 @@ for cxx_ver in g++-{12..9}; do
   fi
 done
 
-echoerr "Using DDPROF_CXX_DEFAULT=${DDPROF_CXX_DEFAULT}"
-echoerr "Using DDPROF_CC_DEFAULT=${DDPROF_CC_DEFAULT}"
-echoerr "Compiler can be overriden with CXX and CC variables when sourcing ${0}"
 
 SCRIPTDIR="$(cd -- $( dirname -- "${BASH_SOURCE[0]}" ) && pwd)" # no "$0" when sourcing
 DDPROF_INSTALL_PREFIX="../deliverables"
@@ -36,18 +33,21 @@ DDPROF_BUILD_BENCH="ON"
 NATIVE_LIB="ON"
 COMPILER_SETTING="-DCMAKE_CXX_COMPILER=\"${CXX:-${DDPROF_CXX_DEFAULT}}\" -DCMAKE_C_COMPILER=\"${CC:-${DDPROF_CC_DEFAULT}}\""
 
+echoerr "Compiler can be overriden with CXX and CC variables when sourcing this script. Current value:"
+echoerr "${COMPILER_SETTING}"
+
 # Avoid having the vendors compiled in the same directory
-EXTENSION_CC=${CC:-"gcc"}
+DDPROF_EXTENSION_CC=${CC:-"gcc"}
 # strip version number from compiler
-EXTENSION_CC=${EXTENSION_CC%-*}
+DDPROF_EXTENSION_CC=${DDPROF_EXTENSION_CC%-*}
 
 LIBC_HAS_MUSL=$(ldd  --version 2>&1  | grep musl || true)
 if [ ! -z "${LIBC_HAS_MUSL}" ]; then
-  LIBC_VERSION=$(${SCRIPTDIR}/tools/get_libc_version.sh musl)
-  EXTENSION_OS="alpine-linux-${LIBC_VERSION}"
+  DDPROF_LIBC_VERSION=$(${SCRIPTDIR}/tools/get_libc_version.sh musl)
+  DDPROF_EXTENSION_OS="alpine-linux-${DDPROF_LIBC_VERSION}"
 else
-  LIBC_VERSION=$(${SCRIPTDIR}/tools/get_libc_version.sh gnu)
-  EXTENSION_OS="unknown-linux-${LIBC_VERSION}"
+  DDPROF_LIBC_VERSION=$(${SCRIPTDIR}/tools/get_libc_version.sh gnu)
+  DDPROF_EXTENSION_OS="unknown-linux-${DDPROF_LIBC_VERSION}"
 fi
 
 DEFAULT_ALLOCATOR_OPT="-DDDPROF_ALLOCATOR=JEMALLOC"
@@ -57,7 +57,7 @@ GetDefaultAllocatorOptions() {
 }
 
 GetDirectoryExtention() {
-  echo "_${EXTENSION_CC}_${EXTENSION_OS}_${1}"
+  echo "_${DDPROF_EXTENSION_CC}_${DDPROF_EXTENSION_OS}_${1}"
 }
 
 COMMON_OPT="${COMPILER_SETTING} ${DEFAULT_ALLOCATOR_OPT} -DACCURACY_TEST=ON -DCMAKE_INSTALL_PREFIX=${DDPROF_INSTALL_PREFIX} -DBUILD_BENCHMARKS=${DDPROF_BUILD_BENCH} -DBUILD_NATIVE_LIB=${NATIVE_LIB}"
@@ -67,9 +67,9 @@ COMMON_OPT="${COMPILER_SETTING} ${DEFAULT_ALLOCATOR_OPT} -DACCURACY_TEST=ON -DCM
 CmakeWithOptions() {
   # Build mode
   # Extra Parameters to cmake
-  BUILD_TYPE=${1}
+  local BUILD_TYPE=${1}
   shift
-  VENDOR_EXTENSION=$(GetDirectoryExtention ${BUILD_TYPE})
+  local VENDOR_EXTENSION=$(GetDirectoryExtention ${BUILD_TYPE})
   # shellcheck disable=SC2086
   cmake_cmd="cmake ${COMMON_OPT} -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DVENDOR_EXTENSION=${VENDOR_EXTENSION} $@"
   echoerr "-------------- cmake command -------------- "
@@ -78,34 +78,34 @@ CmakeWithOptions() {
 }
 
 RelCMake() {
-  BUILD_TYPE=Release
+  local BUILD_TYPE=Release
   CmakeWithOptions ${BUILD_TYPE} $@
 }
 
 DebCMake() {
-    BUILD_TYPE=Debug
+    local BUILD_TYPE=Debug
     CmakeWithOptions ${BUILD_TYPE} $@
 }
 
 SanCMake() {
-    BUILD_TYPE=SanitizedDebug
+    local BUILD_TYPE=SanitizedDebug
     CmakeWithOptions ${BUILD_TYPE} $@
 }
 
 TSanCMake() {
-    BUILD_TYPE=ThreadSanitizedDebug
+    local BUILD_TYPE=ThreadSanitizedDebug
     CmakeWithOptions ${BUILD_TYPE} $@
 }
 
 CovCMake() {
-    BUILD_TYPE=Coverage
+    local BUILD_TYPE=Coverage
     CmakeWithOptions ${BUILD_TYPE} $@
 }
 
 ## Build a directory with a naming that reflects the OS / compiler we are using
 ## Example : mkBuildDir Rel --> build_UB18_clang_Rel
 MkBuildDir() {
-    BUILD_DIR_EXTENSION=$(GetDirectoryExtention ${1})
+    local BUILD_DIR_EXTENSION=$(GetDirectoryExtention ${1})
     echo ${BUILD_DIR_EXTENSION}
     mkdir -p build${BUILD_DIR_EXTENSION} && cd "$_" || exit 1
 }
