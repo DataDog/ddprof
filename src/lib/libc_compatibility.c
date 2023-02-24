bf16ca228c3131aec3e69368c8c38e7f4f53869e
r1viollet <erwan.viollet@datadoghq.com>
2023-02-24 12:08:44 +0100

    OS upgrade - minor fix in the port of the getentropy libc function

--- src/lib/libc_compatibility.c
+++ src/lib/libc_compatibility.c
@@ -1,17 +1,14 @@
-// Unless explicitly stated otherwise all files in this repository are licensed
-// under the Apache License Version 2.0. This product includes software
-// developed at Datadog (https://www.datadoghq.com/). Copyright 2021-Present
-// Datadog, Inc.
 #include <errno.h>
 #include <pthread.h>
+#include <stdlib.h>
 #include <sys/syscall.h> /* Definition of SYS_* constants */
 #include <unistd.h>
 
 int getentropy(void *buffer, size_t len) {
-  int ret;
+  int ret = 0;
   char *pos = buffer;
+
   if (len > 256) {
-    errno = EIO;
     return -1;
   }
 
@@ -20,12 +17,13 @@ int getentropy(void *buffer, size_t len) {
   //  pthread_setcancelstate(PTHREAD_CANCEL_DISABLE, &cs);
 
   while (len) {
-
-    ret = syscall(SYS_getrandom, pos, sizeof(buffer), 0);
+    ret = syscall(SYS_getrandom, pos, len, 0);
     if (ret < 0) {
-      return -1;
+      if (errno == EINTR)
+        continue;
+      else
+        break;
     }
-
     pos += ret;
     len -= ret;
     ret = 0;
