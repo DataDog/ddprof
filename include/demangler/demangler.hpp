4417d1916d5f14a46d037656fe9a6919a9845b7c
David Sanchez <838104+sanchda@users.noreply.github.com>
2023-03-24 06:30:51 -0500

    Better Rust demangling (#230)
    * Add improvements and tests for Rust demangling
    
    ---------
    
    Co-authored-by: sanchda <sanchda@users.noreply.github.com>

---
+++ include/demangler/demangler.hpp
@@ -0,0 +1,15 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0. This product includes software
+// developed at Datadog (https://www.datadoghq.com/). Copyright 2021-Present
+// Datadog, Inc.
+
+#pragma once
+
+#include <string_view>
+
+namespace Demangler {
+
+// Functions
+std::string demangle(const std::string &mangled);
+
+} // namespace Demangler
