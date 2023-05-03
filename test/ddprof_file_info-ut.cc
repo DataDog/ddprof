219e24b6069eb5cbae2543b0bad41ff4d4307c59
r1viollet <erwan.viollet@datadoghq.com>
2023-05-03 13:54:23 +0200

    Fix management of file descriptors
    In the case of whole host, we continuously get new files.
    As we were keeping file descriptors to all the files, we were quickly reaching a limit.

--- test/ddprof_file_info-ut.cc
+++ test/ddprof_file_info-ut.cc
@@ -10,14 +10,8 @@ namespace ddprof {
 TEST(FileInfo, move) {
   LogHandle handle;
   std::string file_path = UNIT_TEST_DATA "/test_int_value.txt";
-  int fd = open(file_path.c_str(), O_RDONLY);
   FileInfo file_info(file_path, 24, 25);
-  FileInfoValue value(std::move(file_info), 1, fd);
-  int dup_fd = value._fd;
-  {
-    auto new_val = std::move(value);
-    EXPECT_EQ(new_val._fd, dup_fd);
-  }
-  EXPECT_EQ(value._fd, -1);
+  FileInfoValue value(std::move(file_info), 1);
+  EXPECT_EQ(value._info._path, file_path);
 }
 } // namespace ddprof
--- test/dwfl_module-ut.cc
+++ test/dwfl_module-ut.cc
@@ -2,7 +2,10 @@
 #include "dwfl_hdr.hpp"
 
 #include <gtest/gtest.h>
+#include <stdio.h>
 #include <string>
+
+#include <filesystem>
 
 #include "ddprof_module.hpp"
 #include "ddprof_module_lib.hpp"
@@ -21,59 +24,77 @@ namespace ddprof {
     (unsigned long)&&__here;                                                   \
   })
 
+namespace fs = std::filesystem;
+
+int cound_fds(pid_t pid) {
+  std::string proc_dir = "/proc/" + std::to_string(pid) + "/fd";
+  int fd_count = 0;
+  for ([[maybe_unused]] const auto &entry : fs::directory_iterator(proc_dir)) {
+    ++fd_count;
+  }
+  return fd_count;
+}
+
 TEST(DwflModule, inconsistency_test) {
+  pid_t my_pid = getpid();
+  int nb_fds_start = cound_fds(my_pid);
+  printf("-- Start open file descriptors: %d\n", nb_fds_start);
   LogHandle handle;
   // Load DSOs from our unit test
   ElfAddress_t ip = _THIS_IP_;
   DsoHdr dso_hdr;
-  pid_t my_pid = getpid();
   DsoHdr::DsoFindRes find_res = dso_hdr.dso_find_or_backpopulate(my_pid, ip);
   // Check that we found the DSO matching this IP
   ASSERT_TRUE(find_res.second);
+  {
+    DwflWrapper dwfl_wrapper;
+    // retrieve the map associated to pid
+    DsoHdr::DsoMap &dso_map = dso_hdr._pid_map[my_pid]._map;
 
-  DwflWrapper dwfl_wrapper;
-  // retrieve the map associated to pid
-  DsoHdr::DsoMap &dso_map = dso_hdr._pid_map[my_pid]._map;
+    for (auto it = dso_map.begin(); it != dso_map.end(); ++it) {
+      Dso &dso = it->second;
+      if (!dso::has_relevant_path(dso._type) || !dso._executable) {
+        continue; // skip non exec / non standard (anon/vdso...)
+      }
 
-  for (auto it = dso_map.begin(); it != dso_map.end(); ++it) {
-    Dso &dso = it->second;
-    if (!dso::has_relevant_path(dso._type) || !dso._executable) {
-      continue; // skip non exec / non standard (anon/vdso...)
+      FileInfoId_t file_info_id = dso_hdr.get_or_insert_file_info(dso);
+      ASSERT_TRUE(file_info_id > k_file_info_error);
+
+      const FileInfoValue &file_info_value =
+          dso_hdr.get_file_info_value(file_info_id);
+      DDProfMod *ddprof_mod =
+          dwfl_wrapper.register_mod(dso._start, dso, file_info_value);
+      EXPECT_TRUE(ddprof_mod->_mod);
+      if (find_res.first == it) {
+        Symbol symbol;
+        GElf_Sym elf_sym;
+        Offset_t lbiais;
+        bool res =
+            symbol_get_from_dwfl(ddprof_mod->_mod, ip, symbol, elf_sym, lbiais);
+        EXPECT_TRUE(res);
+        EXPECT_EQ("ddprof::DwflModule_inconsistency_test_Test::TestBody()",
+                  symbol._demangle_name);
+        EXPECT_EQ(lbiais, ddprof_mod->_sym_bias);
+        FileAddress_t elf_addr = ip - ddprof_mod->_sym_bias;
+        FileAddress_t start_sym, end_sym = {};
+        res = compute_elf_range(elf_addr, elf_sym, start_sym, end_sym);
+        EXPECT_TRUE(res);
+        printf("Start --> 0x%lx - end %lx - lbiais 0x%lx <--\n", start_sym,
+               end_sym, lbiais);
+        EXPECT_GE(elf_addr, start_sym);
+        EXPECT_LE(elf_addr, end_sym);
+
+        // Only expect build-id on this binary (as we can not force it on
+        // others)
+        EXPECT_FALSE(ddprof_mod->_build_id.empty());
+      }
+      // check that we loaded all mods matching the DSOs
+      EXPECT_EQ(ddprof_mod->_status, DDProfMod::kUnknown);
     }
-
-    FileInfoId_t file_info_id = dso_hdr.get_or_insert_file_info(dso);
-    ASSERT_TRUE(file_info_id > k_file_info_error);
-
-    const FileInfoValue &file_info_value =
-        dso_hdr.get_file_info_value(file_info_id);
-    DDProfMod *ddprof_mod =
-        dwfl_wrapper.register_mod(dso._start, dso, file_info_value);
-    EXPECT_TRUE(ddprof_mod->_mod);
-    if (find_res.first == it) {
-      Symbol symbol;
-      GElf_Sym elf_sym;
-      Offset_t lbiais;
-      bool res =
-          symbol_get_from_dwfl(ddprof_mod->_mod, ip, symbol, elf_sym, lbiais);
-      EXPECT_TRUE(res);
-      EXPECT_EQ("ddprof::DwflModule_inconsistency_test_Test::TestBody()",
-                symbol._demangle_name);
-      EXPECT_EQ(lbiais, ddprof_mod->_sym_bias);
-      FileAddress_t elf_addr = ip - ddprof_mod->_sym_bias;
-      FileAddress_t start_sym, end_sym = {};
-      res = compute_elf_range(elf_addr, elf_sym, start_sym, end_sym);
-      EXPECT_TRUE(res);
-      printf("Start --> 0x%lx - end %lx - lbiais 0x%lx <--\n", start_sym,
-             end_sym, lbiais);
-      EXPECT_GE(elf_addr, start_sym);
-      EXPECT_LE(elf_addr, end_sym);
-
-      // Only expect build-id on this binary (as we can not force it on others)
-      EXPECT_FALSE(ddprof_mod->_build_id.empty());
-    }
-    // check that we loaded all mods matching the DSOs
-    EXPECT_EQ(ddprof_mod->_status, DDProfMod::kUnknown);
   }
+  int nb_fds_end = cound_fds(my_pid);
+  printf("-- End open file descriptors: %d\n", nb_fds_end);
+  EXPECT_EQ(nb_fds_start, nb_fds_end);
 }
 
 } // namespace ddprof
