89a2d8492d1c275aae1413e273b6528d107cb106
r1viollet <erwan.viollet@datadoghq.com>
2023-07-21 10:43:27 +0200

    Revert "gcc 13 compilation fix"
    This reverts commit db0d63494419e4a51e968d53f9b08434c810c7cb.

--- include/sha1.h
+++ include/sha1.h
@@ -27,9 +27,9 @@ void SHA1Update(SHA1_CTX *context, const unsigned char *data, uint32_t len);
 
 void SHA1Final(unsigned char digest[20], SHA1_CTX *context);
 
-void SHA1(char *hash_out, const char *str, uint32_t len);
+void SHA1(unsigned char digest[20], const char *str, int len);
 
-void SHA1StrDigest(const char digest[20], char str_digest[41]);
+void SHA1StrDigest(const unsigned char digest[20], char str_digest[41]);
 
 #if defined(__cplusplus)
 }
