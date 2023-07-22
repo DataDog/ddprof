d2b520d6beb1a3a9f9eab38dc81f5c98e9b82856
r1viollet <erwan.viollet@datadoghq.com>
2023-07-22 10:00:23 +0200

    Fix for compilation issue with newer gcc

--- src/ddprof_cli.cc
+++ src/ddprof_cli.cc
@@ -3,6 +3,13 @@
 // developed at Datadog (https://www.datadoghq.com/). Copyright 2021-Present
 // Datadog, Inc.
 
+// gcc 13 issue - remove FORTIFY_SOURCE
+//clang-format off
+// /usr/include/fortify/stdio.h:73:28: error: inlining failed in call to 'always_inline' 'vsnprintf': function body can be overwritten at link time
+// 73 | _FORTIFY_FN(vsnprintf) int vsnprintf(char * _FORTIFY_POS0 __s, size_t __n,
+//clang-format on
+#undef _FORTIFY_SOURCE
+
 #include "ddprof_cli.hpp"
 
 #include "CLI/CLI11.hpp"
