bc67e196b55572b0d5af35b16fdfa3b40c09e299
r1viollet <erwan.viollet@datadoghq.com>
2023-09-06 10:56:51 +0200

    ddprof python support
    Adjust the log to be more comprehensive

--- src/ddprof_process.cc
+++ src/ddprof_process.cc
@@ -31,7 +31,7 @@ austin_handle_t Process::get_austin_handle(pid_t tid) {
   if (!_austin_handle) {
     LG_NTC("Attaching to PID %d/%d", _pid, tid);
     _austin_handle = austin_attach(_pid);
-    LG_NTC("Result of attach %p", _austin_handle);
+    LG_NTC("Result of attach %s", _austin_handle?"Success":"Failure");
   }
   return _austin_handle;
 }
