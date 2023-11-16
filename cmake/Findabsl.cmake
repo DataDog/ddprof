67afa7a56e72def1b2d21d9277a934908c6b3095
Nicolas Savoire <nicolas.savoire@datadoghq.com>
2023-11-16 22:43:33 +0000

    Add abseil as build dependency

---
+++ cmake/Findabsl.cmake
@@ -0,0 +1,16 @@
+find_package(absl CONFIG NO_SYSTEM_ENVIRONMENT_PATH NO_CMAKE_SYSTEM_PATH)
+
+if(absl_FOUND)
+  return()
+endif()
+
+include(FetchContent)
+FetchContent_Declare(
+  absl
+  GIT_REPOSITORY https://github.com/abseil/abseil-cpp.git
+  GIT_TAG 20230802.1)
+
+set(ABSL_PROPAGATE_CXX_STD
+    ON
+    CACHE INTERNAL "")
+FetchContent_MakeAvailable(absl)
