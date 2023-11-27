1a382024673fb6c498b6ba7c5e1ab83ae2eeb719
Nicolas Savoire <nicolas.savoire@datadoghq.com>
2023-11-27 10:23:22 +0100

    Use commit hash instead of tag for Abseil (#345)

--- cmake/Findabsl.cmake
+++ cmake/Findabsl.cmake
@@ -8,7 +8,9 @@ include(FetchContent)
 FetchContent_Declare(
   absl
   GIT_REPOSITORY https://github.com/abseil/abseil-cpp.git
-  GIT_TAG 20230802.1)
+  GIT_TAG fb3621f4f897824c0dbe0615fa94543df6192f30 # Abseil LTS branch, Aug 2023, Patch 1
+                                                   # tag#20230802.1
+)
 
 set(ABSL_PROPAGATE_CXX_STD
     ON
