be63dd57a4d2f70c1d818b95c5305ed691202849
Nicolas Savoire <nicolas.savoire@datadoghq.com>
2024-03-12 14:09:21 +0100

    Fix dependency check (#386)
    * Make sure script fails when dependency file is missing
    * Alpine ddprof should not have any dependency

--- tools/check_dependencies.sh
+++ tools/check_dependencies.sh
@@ -3,6 +3,8 @@
 # Unless explicitly stated otherwise all files in this repository are licensed under the Apache License Version 2.0.
 # This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2021-Present Datadog, Inc.
 
+set -eu
+
 usage() {
     echo "$0 <executable> <expected_dependency_file>"
     echo ""
@@ -15,8 +17,7 @@ if [ $# != 2 ] || [ $1 == "-h" ]; then
     exit 0
 fi
 
-# Omit ld-linux because its full name depends on the architecture
-actual_deps=$(readelf -d "$1" | grep '(NEEDED) *Shared library: \[' | grep -Fv "[ld-linux-" | awk '{ print $5}' | tr -d '[]' | sort -u)
+actual_deps=$(readelf -d "$1" | grep '(NEEDED) *Shared library: \[' | awk '{ print $5 }' | tr -d '[]' | sort -u)
 expected_deps=$(sort -u < "$2")
 
 unexpected_deps=$(comm -13 <(echo "$expected_deps") <(echo "$actual_deps"))
