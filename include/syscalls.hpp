291569bb85e4506eba0ad4b4af7d570bc93731bf
Nicolas Savoire <nicolas.savoire@datadoghq.com>
2024-05-29 13:08:26 +0000

    Add more missing syscalls

--- include/syscalls.hpp
+++ include/syscalls.hpp
@@ -5,13 +5,25 @@
 
 #pragma once
 
+#include <csignal>
 #include <sys/syscall.h>
 #include <unistd.h>
 
 namespace ddprof {
-static inline pid_t gettid() { return syscall(SYS_gettid); }
+inline pid_t gettid() { return syscall(SYS_gettid); }
 
-static inline int memfd_create(const char *name, unsigned int flags) {
+inline int memfd_create(const char *name, unsigned int flags) {
   return syscall(SYS_memfd_create, name, flags);
 }
+
+inline int futex(uint32_t *uaddr, int futex_op, uint32_t val,
+                 const struct timespec *timeout, uint32_t *uaddr2,
+                 uint32_t val3) {
+  return syscall(SYS_futex, uaddr, futex_op, val, timeout, uaddr2, val3);
+}
+
+inline int rt_tgsigqueueinfo(int tgid, int tid, int sig, siginfo_t *uinfo) {
+  return syscall(SYS_rt_tgsigqueueinfo, tgid, tid, sig, uinfo);
+}
+
 } // namespace ddprof
